c1ef7669094e5907f24933d0a604757ec0dfcfae
Vitor B Appolinario <vitorappolinario@gmail.com>
2021-08-22 10:09:13 -0300

    adds basic app components

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Timer from "../components/timer.svelte";
+    import TaskList from "../components/taskList.svelte";
+</script>
+<h1>Appomodoro - Pomodoro time tracking</h1>
+<Timer />
+<TaskList />
