e871f3d1420b8bec5940ab7d9ca031b4ab704303
Vitor Appolinario <vitor@appolinario.com>
2021-09-14 08:24:05 -0300

    Updates smui components

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,9 @@
 /** @type {import('@sveltejs/kit').Config} */
 import adapter from '@sveltejs/adapter-node';
+import preprocess from 'svelte-preprocess';
 
 const config = {
+	preprocess: preprocess({}),
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
